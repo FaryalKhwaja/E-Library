142d715c0dd4e81355a558eeef46fda9eafd6f17
FaryalKhwaja <iamfaryalkhwaja@gmail.com>
2025-08-28 22:47:04 +0500

    Add full logic and beautufil plus easy-to-use UI

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ let addBook = document.querySelector(".AddBookBtn");
 let title = document.querySelector(".title");
 let author = document.querySelector(".author");
 let readStatus = document.querySelector(".readStatus");
+let openForm = document.querySelector(".OpenFormBtn");
+let form = document.getElementById("bookForm");
 
 function Book (title, author, readStatus) {
     this.title = title;
@@ -12,20 +14,33 @@ function Book (title, author, readStatus) {
     this.readStatus = readStatus;
 }
 
-function addBookToLibrary (title, author, readStatus) {
+function addBookToLibrary(title, author, readStatus) {
     const book = new Book(title, author, readStatus);
     library.push(book);
-    let bookDiv = document.createElement("div");
-    let bookTitle = title;
-    let bookAuthor = author;
-    let bookReadStatus = readStatus;
-    bookDiv.textContent = title + "\n" + author + "\nRead: " + readStatus;
-    bookDiv.style.whiteSpace = "pre-line";
-    shelfDiv.appendChild(bookDiv);
+
+    shelfDiv.innerHTML = "";
+
+    library.forEach((b) => {
+        const bookDiv = document.createElement("div");
+        bookDiv.classList.add("bookDiv");
+        bookDiv.textContent = `${b.title}\n\n${b.author}\n\nRead: ${b.readStatus}`;
+        bookDiv.style.whiteSpace = "pre-line";
+        bookDiv.style.width = `${100 / library.length}%`;
+        bookDiv.style.boxSizing = "border-box";
+
+        shelfDiv.appendChild(bookDiv);
+    });
+
     console.log(library);
+    form.style.display = "none";
 }
 
 addBook.addEventListener("click", () => {
     addBookToLibrary(title.value, author.value, readStatus.value);
 });
 console.log(addBook);
+
+
+openForm.addEventListener("click", () => {
+    form.style.display = "flex";
+});
